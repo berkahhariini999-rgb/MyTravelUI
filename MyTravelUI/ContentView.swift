960e8a41091de43e46eeaa807d76f693e07243bb
berkahhariini999-rgb <berkahhariini999@gmail.com>
2025-10-29 06:43:07 +0700

    Initial Commit

---
+++ MyTravelUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTravelUI
+//
+//  Created by Iqbal Alhadad on 29/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTravelUI/MyTravelUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTravelUIApp.swift
+//  MyTravelUI
+//
+//  Created by Iqbal Alhadad on 29/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MyTravelUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
